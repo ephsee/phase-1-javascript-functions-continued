d58dbed776fa6db4cadea1fbffdc33a105afa007
Francois Spies <fcspies@gmail.com>
2022-01-11 10:48:20 -0500

    commit tree 3efcc691d63414af379c76af03b57b2f6bc7ad00

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// mondayWork('work from home')
+function mondayWork(activity = 'go to the office') {
+    return "This Monday, I will " + activity + ".";
+}
+mondayWork()
+
+function wrapAdjective(makeNice = '*') {
+    return function(par = 'special'){
+        return `You are ${makeNice}${par}${makeNice}!`
+    }
+}
+wrapAdjective()
